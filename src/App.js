be6e23f69f1863d2128cb6d633de394b30fedd52
Eloghosa Osagie <st.eloghosa@gmail.com>
2022-09-03 04:26:51 +0100

    cleaned up code

---
+++ src/App.js
@@ -0,0 +1,175 @@
+import React from "react";
+//import ReactDOM from 'react-dom/client';
+
+import ChatBot from "react-simple-chatbot";
+import { ThemeProvider } from "styled-components";
+import "./index.css";
+
+//theme
+const theme = {
+  background: "#091E42DE",
+  fontFamily: "Helvetica Neue",
+  headerBgColor: "#D51709",
+  headerFontColor: "#FFF",
+  headerFontSize: "20px",
+  botBubbleColor: "#00",
+  botFontColor: "#fff",
+  userBubbleColor: "#fff",
+  userFontColor: "#4a4a4a",
+};
+
+const steps = [
+  {
+    id: "1",
+    message: "Hello, is there anything I can help you with today?",
+    trigger: "2",
+  },
+  {
+    id: "2",
+    user: true,
+    trigger: "3",
+  },
+  {
+    id: "3",
+    message:
+      "can you provide your full the following details: name, phone number and date of birth",
+    trigger: "4",
+  },
+  {
+    id: "4",
+    message: "what is your date of birth? e.g 09/10/1994 dd/mm/yy ",
+    trigger: "5",
+  },
+  {
+    id: "5",
+    user: true,
+    trigger: "6",
+  },
+  {
+    id: "6",
+    message: "what is your phone number? ",
+    trigger: "7",
+  },
+  {
+    id: "7",
+    user: true,
+    trigger: "8",
+  },
+  {
+    id: "8",
+    message: "what is your name? ",
+    trigger: "9",
+  },
+  {
+    id: "9",
+    user: true,
+    trigger: "10",
+  },
+  {
+    id: "10",
+    message:
+      "Thank you so much {previousValue}, I have been able to Authenticate your identity. Kindly choose what you want me to help you with today ",
+    trigger: "11",
+  },
+  {
+    id: "11",
+    options: [
+      { value: "1", label: "Check Balance", trigger: "12" },
+      { value: "2", label: "Account Opening", trigger: "13" },
+      { value: "3", label: "Buy Airtime", trigger: "14" },
+      { value: "4", label: "Send Money", trigger: "17" },
+      { value: "5", label: "Buy Data", trigger: "1" },
+      { value: "6", label: "Complain", trigger: "1" },
+      { value: "7", label: "Get Statement", trigger: "1" },
+      { value: "8", label: "Self Service", trigger: "1" },
+      { value: "9", label: "Link New Account", trigger: "1" },
+      { value: "10", label: "Reset Pin", trigger: "1" },
+      { value: "11", label: "Loan Request", trigger: "1" },
+      { value: "12", label: "FAQ", trigger: "1" },
+    ],
+  },
+  {
+    id: "12",
+    message: "Your current account balance is : $4000.85",
+    trigger: 'continue-chat',
+  },
+  {
+    id: "13",
+    message:
+      "Visit the nearest branch to you, to enable you open an account. OR visit our website https://testbank.com",
+    trigger: 'continue-chat',
+  },
+  {
+    id: "14",
+    message: "Input number with country code for airtime",
+    trigger: "15",
+  },
+  {
+    id: "15",
+    user: true,
+    trigger: "16",
+  },
+  {
+    id: "16",
+    message: "Airtime sent to {previousValue}",
+    trigger: 'continue-chat',
+  },
+
+  {
+    id: "17",
+    message: "Input account number",
+    trigger: "18",
+  },
+  {
+    id: "18",
+    user: true,
+    trigger: "19",
+  },
+  {
+    id: "19",
+    message:
+      "This account number {previousValue} has successfully been credited.",
+    trigger: 'continue-chat',
+  },
+  {
+    id:'continue-chat', 
+    message:'Do you want to perform another transaction?', 
+    trigger:'end-chat',
+   },
+  {
+    id: "end-chat",
+    options: [
+      { value: "y", label: "Yes", trigger: "yes-response" },
+      { value: "n", label: "No", trigger: "no-response" },
+    ],
+  },
+  {
+    id: 'yes-response',
+    message:"Greate !",
+    trigger: "11"
+  },
+  {
+    id: 'no-response',
+    message: "Thanks for using my service!",
+    end: true
+  },
+
+
+  
+];
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <ChatBot
+          steps={steps}
+          headerTitle="Customer Service"
+          className="container mt-40 px-3 md:ml-40 md:pl-10 "
+        />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,97 +1,12 @@
-import React from 'react';
+import React from "react";
 //import ReactDOM from 'react-dom/client';
 import { render } from "react-dom";
-import ChatBot from 'react-simple-chatbot';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
 
 
-const steps =[
-  {
-    id: '1',
-    message: 'What is your name ?',
-    trigger: '2',
-  },
-  {
-    id: '2',
-   user: true,
-    trigger: '3',
-  },
-  {
-    id: '3',
-  message : 'Hi {previousValue}, nice to meet you! Welcome to KeyStone Bank',
-    trigger: '4',
-  },
-  {
-    id: '4',
-    message : 'How may I help you',
-    trigger: '5',
-  },
-  {
-    id: '5',
-    options: [
-      { value: '1', label: 'Check Balance', trigger: '6' },
-      { value: '2', label: 'Account Opening', trigger: '7' },
-      { value: '3', label: 'Buy Airtime', trigger: '8' },
-      { value: '4', label: 'Send Money', trigger: '11' },
-      { value: '5', label: 'Buy Data', trigger: '3' },
-      { value: '6', label: 'Complain', trigger: '3' },
-      { value: '7', label: 'Get Statement', trigger: '3' },
-      { value: '8', label: 'Self Service', trigger: '3' },
-      { value: '9', label: 'Link New Account', trigger: '3' },
-      { value: '10', label: 'Reset Pin', trigger: '3' },
-      { value: '11', label: 'Loan Request', trigger: '3' },
-      { value: '12', label: 'FAQ', trigger: '3' },
-    ],
-  },
-  {
-    id: '6',
-    message : 'Your current account balance is : $4000.85',
-    trigger: '5',
-  },
-  {
-    id: '7',
-    message : 'Visit the nearest branch to you, to enable you open an account. OR visit our website https://testbank.com',
-    trigger: '5',
-  },
-  {
-    id: '8',
-    message : 'Input number with country code for airtime',
-    trigger: '9',
-  },
-  {
-    id: '9',
-   user: true,
-    trigger: '10',
-  },
-  {
-    id: '10',
-  message : 'Airtime sent to {previousValue} ',
-    trigger: '5',
-  },
-  {
-    id: '11',
-    message : 'Input account number',
-    trigger: '12',
-  },
-  {
-    id: '12',
-   user: true,
-    trigger: '13',
-  },
-  {
-    id: '13',
-  message : 'Money sent to {previousValue} ',
-    trigger: '5',
-  },
-  {
-    id: '25',
-    message: 'Thanks for using my service!',
-    end: true,
-  },
-]
-
-
-render(<ChatBot steps={steps} className="container mt-40 px-3 md:ml-40 md:pl-10" />, document.getElementById("root"));
-
-
+render(
+  <App />,
+  document.getElementById("root")
+);
